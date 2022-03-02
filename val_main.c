adb098aa5a9b885b12ac7066811e4dafcbac08e4
Ilshat Galimov <86232244+i-galimov@users.noreply.github.com>
2022-03-02 16:04:27 +0300

    Add files via upload

--- val_main.c
+++ val_main.c
@@ -4,8 +4,6 @@
 # include <stdio.h>
 
 // struct
-int		counter;
-
 typedef struct s_list
 {
 	int				value;
@@ -23,13 +21,6 @@ typedef struct s_all
 }			t_all;
 
 // errors.c
-
-void    error(void)
-{
-    write(1, "Error\n", 6);
-    exit(1);
-}
-
 void    ft_arg_error(void)
 {
     write(1, "Incorrect arguments\n", 20);
@@ -152,7 +143,6 @@ void	ft_sa(t_list	**a)
 	tmp->next = (*a);
 	*a = tmp;
 	write(1, "sa\n", 3);
-	counter++;
 }
 
 void	ft_sb(t_list	**b)
@@ -164,7 +154,6 @@ void	ft_sb(t_list	**b)
 	tmp->next = (*b);
 	*b = tmp;
 	write(1, "sb\n", 3);
-	counter++;
 }
 
 void	ft_ss(t_list	**a, t_list	**b)
@@ -180,7 +169,6 @@ void	ft_ss(t_list	**a, t_list	**b)
 	tmp->next = (*b);
 	*b = tmp;
 	write(1, "ss\n", 3);
-	counter++;
 }
 
 void	ft_pb(t_list **a, t_list **b)
@@ -194,7 +182,6 @@ void	ft_pb(t_list **a, t_list **b)
 	tmp->next = *b;
 	*b = tmp;
 	write(1, "pb\n", 3);
-	counter++;
 }
 
 void	ft_pa(t_list **a, t_list **b)
@@ -208,7 +195,6 @@ void	ft_pa(t_list **a, t_list **b)
 	tmp->next = *a;
 	*a = tmp;
 	write(1, "pa\n", 3);
-	counter++;
 }
 // rotate.c
 void	ft_rra(t_list **a)
@@ -225,7 +211,6 @@ void	ft_rra(t_list **a)
 	*a = last_prev->next;
 	last_prev->next = 0;
 	write(1, "rra\n", 4);
-	counter++;
 }
 
 void	ft_rrb(t_list **b)
@@ -242,7 +227,6 @@ void	ft_rrb(t_list **b)
 	*b = last_prev->next;
 	last_prev->next = 0;
 	write(1, "rrb\n", 4);
-	counter++;
 }
 
 void	ft_rrr(t_list **a, t_list **b)
@@ -270,7 +254,6 @@ void	ft_rrr(t_list **a, t_list **b)
 	}
 	if ((*a && (*a)->next) || (*b && (*b)->next))
 		write(1, "ft_rrr\n", 4);
-		counter++;
 }
 
 void	ft_ra(t_list **a)
@@ -288,7 +271,6 @@ void	ft_ra(t_list **a)
 	last->next = tmp;
 	tmp->next = 0;
 	write(1, "ra\n", 3);
-	counter++;
 }
 
 void	ft_rb(t_list **b)
@@ -306,7 +288,6 @@ void	ft_rb(t_list **b)
 	last->next = tmp;
 	tmp->next = 0;
 	write(1, "rb\n", 3);
-	counter++;
 }
 
 // rr.c
@@ -337,7 +318,80 @@ void	ft_rr(t_list **a, t_list **b)
 	}
 	if (((*a) && (*a)->next) || ((*b) && (*b)->next))
 		write(1, "rr\n", 3);
-		counter++;
+}
+
+// sort_array.c
+int    count_list(t_list *lst)
+{
+    int    i;
+
+    i = 1;
+    while (lst->next)
+    {
+        lst = lst->next;
+        i++;
+    }
+    return (i);
+}
+
+void    define_index(t_list **a, int *arr)
+{
+    t_list    *tmp;
+    int        i;
+    int        size;
+
+    size = count_list(*a);
+    tmp = *a;
+    while (tmp)
+    {
+        i = 0;
+        while (i < size)
+        {
+            if (tmp->value == arr[i])
+            {
+                tmp->index = i;
+                break ;
+            }
+            i++;
+        }
+        tmp = tmp->next;
+    }
+}
+
+void    ft_swap(int    *a, int *b)
+{
+    int    tmp;
+
+    tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+void    sort_array(int argc, char **argv, t_list **a)
+{
+    int        *arr;
+    int        i;
+    int        j;
+
+    i = 0;
+    j = 1;
+    arr = malloc(sizeof(int) * (argc - 1));
+    while (j < argc)
+        arr[i++] = ft_atoi(argv[j++]);
+    i = 0;
+    while (i != argc - 1)
+    {
+        j = 0;
+        while (j != argc - 2)
+        {
+            if (arr[j] > arr[j + 1])
+                ft_swap(&arr[j], &arr[j + 1]);
+            j++;
+        }
+        i++;
+    }
+    define_index(a, arr);
+    free(arr);
 }
 
 // sort_many_num.c
@@ -424,80 +478,6 @@ void	sort_many_num(t_all *s)
 	}
 }
 
-// sort_array.c
-int    count_list(t_list *lst)
-{
-    int    i;
-
-    i = 1;
-    while (lst->next)
-    {
-        lst = lst->next;
-        i++;
-    }
-    return (i);
-}
-
-void    define_index(t_list **a, int *arr)
-{
-    t_list    *tmp;
-    int        i;
-    int        size;
-
-    size = count_list(*a);
-    tmp = *a;
-    while (tmp)
-    {
-        i = 0;
-        while (i < size)
-        {
-            if (tmp->value == arr[i])
-            {
-                tmp->index = i;
-                break ;
-            }
-            i++;
-        }
-        tmp = tmp->next;
-    }
-}
-
-void    ft_swap(int    *a, int *b)
-{
-    int    tmp;
-
-    tmp = *a;
-    *a = *b;
-    *b = tmp;
-}
-
-void    sort_array(int argc, char **argv, t_list **a)
-{
-    int        *arr;
-    int        i;
-    int        j;
-
-    i = 0;
-    j = 1;
-    arr = malloc(sizeof(int) * (argc - 1));
-    while (j < argc)
-        arr[i++] = ft_atoi(argv[j++]);
-    i = 0;
-    while (i != argc - 1)
-    {
-        j = 0;
-        while (j != argc - 2)
-        {
-            if (arr[j] > arr[j + 1])
-                ft_swap(&arr[j], &arr[j + 1]);
-            j++;
-        }
-        i++;
-    }
-    define_index(a, arr);
-    free(arr);
-}
-
 // sort_by_three.c
 static void    case_sort1(t_list **a)
 {
@@ -563,25 +543,25 @@ int    ft_find_min(t_list **a)
 
 void    ft_moves(t_list **a, t_list **b, int argc)
 {
-    int     small;
+    int     min;
     t_list  *tmp;
 
     tmp = *a;
-    small = ft_find_min(a);
-    if (tmp->next->value == small)
+    min = ft_find_min(a);
+    if (tmp->next->value == min)
         ft_sa(a);
-    else if (tmp->next->next->value == small)
+    else if (tmp->next->next->value == min)
     {
         ft_ra(a);
         ft_ra(a);
     }
-    else if (tmp->next->next->next->value == small && argc == 6)
+    else if (tmp->next->next->next->value == min && argc == 6)
     {
         ft_rra(a);
         ft_rra(a);
     }
-    else if ((tmp->next->next->next->value == small && argc == 5) \
-    || (tmp->value != small && argc == 6))
+    else if ((tmp->next->next->next->value == min && argc == 5) \
+    || (tmp->value != min && argc == 6))
         ft_rra(a);
     ft_pb(a, b);
 }
@@ -628,6 +608,19 @@ void    free_list(t_all **head)
         free_list(&(*head)->next);
     free(*head);
 }
+
+int	len_list(t_list *a)
+{
+	int	n;
+
+	n = 0;
+	while (a)
+	{
+		n++;
+		a = a->next;
+	}
+	return (n);
+}
 // push_swap.c
 
 void    type_sort(t_list **a, t_list **b, t_all *s, int argc)
@@ -642,43 +635,21 @@ void    type_sort(t_list **a, t_list **b, t_all *s, int argc)
         sort_many_num(s);
 }
 
-void    push_swap(int argc, char **argv)
-{
-    t_list    *a;
-    t_list    *b;
-    t_all   *s;
-
-    
-    if (ft_check_input(argv))
-    {
-        write(1, "Error: input is wrong, try again\n", 33);
-        exit(1);
-    }
-    if (ft_check_sorted(argv))
-        exit(0);
-    create_list(&a, argv);
-    
-    type_sort(&a, &b, &s, argc);
-    free_list(&a);
-}
-
 int	main(int argc, char **argv)
 {
-    t_list  a;
-    t_list  b;
     t_all   s;
 
-    if (argc == 1)
-        exit(0);
-    printf("%d\n", ft_check_dublicate(argv));
-    printf("%d\n", ft_check_overflow(argv));
-    printf("%d\n", ft_check_sorted(argv));
+	s.next = 0;
+	s.b = 0;
+	s.num = len_list(s.a);
+    if (argc < 2)
+        ft_arg_error();
 	if (ft_check_input(argv) == 1)
         ft_arg_error();
-    create_list(&a, argv);
-    sort_array(argc, argv, &a);
-    type_sort(&a, &b, &s, argc);
-    free_list(&a);
+    create_list(s.a, argv);
+    sort_array(argc, argv, s.a);
+    type_sort(s.a, s.a, s, argc);
+    free_list(s.a);
 
     return (0);
 }
